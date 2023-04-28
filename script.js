8653ea190d4bfd3b9124bbd09a0c7897cd5aaa2f
JBalane <jayarbalane1313@gmail.com>
2023-04-28 11:25:53 +0800

    finalized capstone

--- script.js
+++ script.js
@@ -11,8 +11,11 @@ MenuBtn.addEventListener('click', function(e) {
 
 let typed = new Typed('.auto-input',{
 	strings: ['Front-End Developer!','Back-End Developer!','Full-stack Developer!', 'Streamer!'],
-	typeSpeed: 100,
-	backSpeed: 100,
+	typeSpeed: 70,
+	backSpeed: 70,
 	backDelay: 2000,
 	loop: true,
 })
+
+// Active link state on scroll
+
